b14a08cb4fcb21863faf6d8ccd53e57fce7f92ad
txrwall <wllcblm@gmail.com>
2021-01-27 23:44:42 -0300

    deploy basic structure

---
+++ server/test/posts.test.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+test('Should get posts', async function () {
+  const response = await axios({
+    url: 'http://localhost:3000/posts',
+    method: 'get',
+  });
+
+  const posts = response.data;
+  expect(posts).toHaveLength(3);
+  const [firstPost, secondPost] = posts;
+  expect(secondPost.id).toBe(4);
+});
